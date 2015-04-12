787a154bc92e6ad6951a2e17c546fea6c80a55c9
dhruv.kalaria <dhruvkalaria@gmail.com>
2015-04-12 02:58:11 -0700

    Initial Commit with following changes: 1. Created Spash Screen, Login Screen and Sign Up Screen

---
+++ GroceryPickup/src/com/example/grocerypickup/Home.java
@@ -0,0 +1,14 @@
+package com.example.grocerypickup;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity{
+
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_activity);
+	}
+
+}
---
+++ GroceryPickup/src/com/example/grocerypickup/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.grocerypickup;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+			//Thread for displaying the splash Screen
+			Thread splash_screen = new Thread()	{
+				
+				public void run()	{
+					try{
+						sleep(2000);
+					}catch(Exception e){
+						e.printStackTrace();
+					}finally{
+						startActivity(new Intent(getApplicationContext(),Main.class));
+						finish();
+					}
+				}
+			};
+			splash_screen.start();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
